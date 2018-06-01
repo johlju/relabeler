70cd5a20dfea0a75b4538d1d52104d0de95eb414
Johan Ljunggren <johan.ljunggren@shamn.se>
2018-06-01 17:47:16 +0200

    Update events

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 A GitHub App (built with [probot](https://github.com/probot/probot)) that is
 able to label issues and pull requests on events.
 
+# Install
+
+Install [relabeler (Development)](https://github.com/apps/relabeler-development)
+
 ## Setup
 
 ```sh
--- index.js
+++ index.js
@@ -5,6 +5,35 @@ module.exports = (robot) => {
   // For more information on building apps:
   // https://probot.github.io/docs/
 
-  // To get your app running against GitHub, see:
+  robot.on(['installation.created'], async context => {
+    robot.log.debug('Installation event!')
+
+    const action = context.payload.action
+
+    robot.log.debug(action)
+    robot.log.debug(context.payload.installation.repository_selection)
+    robot.log.debug(context.payload.repositories)
+  })
+
+  robot.on(['integration_installation.created'], async context => {
+    robot.log.debug('integration_installation event!')
+    robot.log(context.payload.repositories)
+    robot.log(context.payload.installation.repository_selection)
+  })
+
+// To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
+
+  robot.on(['issues.opened', 'issues.edited', 'issue_comment.created', 'issue_comment.deleted'], async context => {
+    // An issue was just opened.
+    robot.log('Got an event!')
+    robot.log(context)
+  })
+
+  robot.on(['status'], async context => {
+    // An issue was just opened.
+    robot.log('Got an event!')
+    robot.log(context)
+  })
+
 }
