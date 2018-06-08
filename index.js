709cf829422b15338aed2a05c95d74bfad04527c
Johan Ljunggren <johan.ljunggren@shamn.se>
2018-06-08 11:31:42 +0200

    Adding more test code

--- index.js
+++ index.js
@@ -6,54 +6,139 @@ module.exports = (robot) => {
   // https://probot.github.io/docs/
 
   robot.on(['installation.created'], async context => {
-    robot.log.debug('installation event!')
-
     const action = context.payload.action
-    robot.log.debug('Action: %s', action)
+    robot.log.debug('installation event! Action: %s', action)
 
     robot.log.debug(context.payload.installation.repository_selection)
     robot.log.debug(context.payload.repositories)
   })
 
   robot.on(['integration_installation.created'], async context => {
-    robot.log.debug('integration_installation event!')
-
     const action = context.payload.action
-    robot.log.debug('Action: %s', action)
+    robot.log.debug('integration_installation event! Action: %s', action)
 
     robot.log.debug(context.payload.installation.repository_selection)
     robot.log.debug(context.payload.repositories)
   })
 
-  robot.on(['pull_request.opened', 'pull_request.closed', 'pull_request.edited', 'pull_request.labeled'], async context => {
-    robot.log.debug('pull_request event!')
+  // 'pull_request.labeled', 'pull_request.unlabeled' (must handle state === 'open')
+  // pull_request.synchronize, 'pull_request.reopened', 'pull_request.edited'
+  robot.on(['pull_request.opened'], async context => {
+    if (context.payload.pull_request.state === 'open') {
+      const config = await loadConfig(context)
+      robot.log.debug('Configuration: %s', JSON.stringify(config))
+
+      const action = context.payload.action
+      robot.log.debug('pull_request event! Action: %s', action)
+
+      const isBot = context.isBot
+      robot.log.debug('pull_request action was bot: %s', isBot)
+
+      // https://developer.github.com/v4/reference/enum/commentauthorassociation/
+      // "author_association": "NONE","OWNER",
+      robot.log.debug('Association: %s', context.payload.pull_request.author_association)
+
+      // https://developer.github.com/changes/2018-05-30-end-jean-grey-preview/
+      // robot.log.debug('Global node ID: %s', context.payload.node_id)
+
+      if (config.perform) {
+        context.github.issues.createComment(context.issue({ body: 'hej' }))
+      } else {
+        robot.log.debug('dry-run: Would have written a comment to PR #%s', context.payload.number)
+      }
+
+      const label = 'needs review'
+
+      if (config.perform) {
+        context.github.issues.addLabels(context.issue({ labels: [label] }))
+      } else {
+        robot.log.debug('dry-run: Would have labeled PR #%s with a label \'%s\'', context.payload.number, label)
+      }
+    } else {
+      robot.log.debug('Will not do any actions on closed pull request #%s.', context.payload.pull_request.number)
+    }
+  })
+
+  robot.on(['pull_request.closed'], async context => {
+    const config = await loadConfig(context)
+    robot.log.debug('Configuration: %s', JSON.stringify(config))
 
     const action = context.payload.action
-    robot.log.debug('Action: %s', action)
+    robot.log.debug('pull_request event! Action: %s', action)
+
+    const isBot = context.isBot
+    robot.log.debug('pull_request action was bot: %s', isBot)
 
     // https://developer.github.com/v4/reference/enum/commentauthorassociation/
     // "author_association": "NONE","OWNER",
     robot.log.debug('Association: %s', context.payload.pull_request.author_association)
+
+    // https://developer.github.com/changes/2018-05-30-end-jean-grey-preview/
+    // robot.log.debug('Global node ID: %s', context.payload.node_id)
+
+    if (config.perform) {
+      // context.github.issues.createComment(context.issue({body: 'hej'}))
+    } else {
+      robot.log.debug('dry-run: Would have written a comment to PR #%s', context.payload.number)
+    }
+
+    const label = 'needs review'
+
+    if (config.perform) {
+      // context.github.issues.addLabels(context.issue({labels: [label]}))
+    } else {
+      robot.log.debug('dry-run: Would have labeled PR #%s with a label \'%s\'', context.payload.number, label)
+    }
   })
 
   robot.on(['issues.opened', 'issues.edited', 'issue_comment.created', 'issue_comment.deleted'], async context => {
-    // An issue was just opened.
-    robot.log.debug('issues event!')
+    // TODO: only on opened PR's.
+    if (context.payload.issue.state === 'open') {
+      // An issue was just opened.
+      const action = context.payload.action
+      robot.log.debug('issues event! Action: %s', action)
 
-    const action = context.payload.action
-    robot.log.debug('Action: %s', action)
+      const isBot = context.isBot
+      robot.log.debug('issues action was bot: %s', isBot)
 
-    robot.log.debug(context)
+      robot.log.trace(context)
+    } else {
+      robot.log.debug('Will not do any actions on closed issue or pull request (number #%s).', context.payload.issue.number)
+    }
   })
 
   robot.on(['status'], async context => {
     const action = context.payload.action
     robot.log.debug('Action: %s', action)
 
+    const isBot = context.isBot
+    robot.log.debug('status action was bot: %s', isBot)
+
     // An issue was just opened.
     robot.log.debug('status event!')
-    robot.log.debug(context)
+    robot.log.trace(context)
   })
+
+  async function loadConfig (context) {
+    const repoConfig = await context.config('relabeler.yml')
+
+    let config
+
+    if (!repoConfig) {
+      // Do not activate - do dry-run
+      config = {
+        perform: false
+      }
+    } else {
+      config = {
+        perform: true,
+        ...repoConfig
+      }
+    }
+
+    console.log(config)
+    return config
+  }
 }
 
 // Check npm: eslint, codecov, chai
--- test/index.test.js
+++ test/index.test.js
@@ -1,19 +1,134 @@
+// Just used to Describe (What), Context (When), It (Should)
+const context = describe
+
+const fs = require('fs');
+const expect = require('expect')
+
+// Requiring probot allows us to mock out a robot instance
+const { createRobot } = require('probot')
+// Requiring our app
+const app = require('..')
+
+function readMockConfig (fileName) {
+  let config
+
+  try {
+    config = fs.readFileSync(__dirname + '/configs/' + fileName, 'utf8')
+    //console.log(repoConfig)
+  } catch (e) {
+    console.log(e)
+  }
+
+  return config
+}
+
 // You can import your modules
 // const index = require('../index')
 
-const context = describe
+// Create a fixtures folder in your test folder
+// Then put any larger testing payloads in there
+const payload = {
+  pullRequest: {
+    openedAsOwner: require('./fixtures/pull_request.opened.owner'),
+    openedAsFirstTimer: require('./fixtures/pull_request.opened.first_timer')
+  }
+}
 
-beforeEach(() => {
-})
+describe('relabeler', () => {
+  let robot
+  let github
+  let configData
 
-afterEach(() => {
-})
+  beforeEach(() => {
+    // Here we create a robot instance
+    robot = createRobot()
 
-describe('addition', () => {
-  context('When One and Two and Three are added', () => {
-    it('Should be 6', () => {
-      // your real tests go here
-      expect(1 + 2 + 3).toBe(6)
+    // Here we initialize the app on the robot instance
+    app(robot)
+
+    // This is an easy way to mock out the GitHub API
+    // mocks context.github*
+    github = {
+      issues: {
+        createComment: jest.fn().mockReturnValue(Promise.resolve({
+          // Whatever the GitHub API should return
+        })),
+        addLabels: jest.fn().mockReturnValue(Promise.resolve({
+          // Whatever the GitHub API should return
+        }))
+      },
+      repos: {
+        // This is to mock that a config is loaded.
+        // await context.config('relabeler.yml') is reding the key getContent.
+        //
+        // configData is set in the beforeAll()-block in each context.
+        // getContent: jest.fn().mockReturnValue(Promise.resolve(configData))
+        getContent: jest.fn().mockReturnValue(Promise.resolve(configData))
+      }
+    }
+
+    // Passes the mocked out GitHub API into out robot instance
+    robot.auth = () => Promise.resolve(github)
+  })
+
+  context('When pull request is opened', () => {
+    context('When executing', () => {
+      beforeAll(() => {
+        let repoYamlConfig = readMockConfig('onPullRequestOpen.yml')
+
+        configData = {
+          data: {
+            // This must be encoded in base64
+            content: Buffer.from(repoYamlConfig).toString('base64')
+          }
+        }
+      })
+
+      it('Should set the correct label', async () => {
+        await robot.receive(payload.pullRequest.openedAsOwner)
+        expect(github.issues.addLabels).toHaveBeenCalledWith({
+          labels: ['needs review'],
+          number: 11,
+          owner: 'johlju',
+          repo: 'DebugApps'
+        })
+      })
+
+      it('Should write correct comment', async () => {
+        await robot.receive(payload.pullRequest.openedAsOwner)
+        expect(github.issues.createComment).toHaveBeenCalledWith({
+          body: 'hej',
+          number: 11,
+          owner: 'johlju',
+          repo: 'DebugApps'
+        })
+      })
+    })
+
+    context('When on dry-run', () => {
+      beforeAll(() => {
+        configData = {
+          data: {
+            // This must be encoded in base64
+            // TODO: There must be away to mock await context.config('relabeler.yml')
+            //       instead of mocking that we are returning the below as the
+            //       configuration.
+            content: Buffer.from('perform: false').toString('base64')
+          }
+        }
+      })
+
+      it('Should not set the correct label', async () => {
+        await robot.receive(payload.pullRequest.openedAsOwner)
+
+        expect(github.issues.addLabels).not.toHaveBeenCalled()
+      })
+
+      it('Should not write correct comment', async () => {
+        await robot.receive(payload.pullRequest.openedAsOwner)
+
+        expect(github.issues.createComment).not.toHaveBeenCalled()
+      })
     })
   })
 })
